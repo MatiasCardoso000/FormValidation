b6d161c92676e3753462a2d9d84ffc212dbaa837
MatiasC5 <cardosomatias1994@gmail.com>
2024-10-08 15:14:28 -0300

    Form validation

---
+++ Validacion-Formularios/Formulario-1/script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("form");
+const email = document.getElementById("email");
+const emailError = document.querySelector("#email + span.error");
+
+email.addEventListener("input", () => {
+  if (email.validity.valid) {
+    emailError.textContent = "";
+    emailError.className = "error";
+  } else {
+    showError();
+  }
+});
+
+form.addEventListener("submit", (e) => {
+  if (!email.validity.valid) {
+    showError();
+    e.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    emailError.textContent = "You need to enter an email address.";
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = "Entered value needs to be an email address.";
+  }
+
+  emailError.className = "error active";
+  email.className = "error-email";
+}
