6d29e18a86a2810e008585fc7dab9123e16b7d2f
MatiasC5 <cardosomatias1994@gmail.com>
2024-10-08 15:54:50 -0300

    Cambio estilo del input y el mensaje de error

--- Validacion-Formularios/Formulario-1/script.js
+++ Validacion-Formularios/Formulario-1/script.js
@@ -23,6 +23,8 @@ function showError() {
     emailError.textContent = "You need to enter an email address.";
   } else if (email.validity.typeMismatch) {
     emailError.textContent = "Entered value needs to be an email address.";
+  } else if (email.validity.tooShort) {
+    emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
   }
 
   emailError.className = "error active";
